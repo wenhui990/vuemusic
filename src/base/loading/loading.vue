ef46be43c4a74436a2eaaba0bea61069deee87ca
温慧 <729250771@qq.com>
2018-12-14 22:39:43 +0800

    热门歌单推荐完成-图片懒加载-loading动画

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading">
+    <img src="./loading.gif" width="24" height="24" alt="加载中...">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: '正在载入...'
+      }
+    },
+    mounted() {
+      console.log(this.title)
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .loading
+    width: 100%
+    text-align: center
+    .desc
+      line-height: 30px
+      font-size: $font-size-small
+      color: $color-text-l
+
+</style>
---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,64 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: [1]
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initScroll()
+      }, 20)
+    },
+    methods: {
+      _initScroll() {
+        if (!this.$refs.wrapper) {
+          return
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+        console.log(222)
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      disable() {
+        this.scroll && this.scroll.disable()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      }
+    },
+    watch: {
+      data() {
+        console.log(1111)
+        setTimeout(() => {
+          this.refresh()
+        }, 20)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
